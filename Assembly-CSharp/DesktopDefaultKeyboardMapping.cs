a039308e392c5d2311a1273e94b53842daed252d
kogama-history <kogama-history@openkogama.invalid>
2018-03-07 14:02:55 +0000

    1.81.8 (5.3.8p2)
    Build-Id: 52468427-306f-46ce-96be-9e7596b7ce50
    Build-Sha256: f87d47152dd789be676970030ba4bca38473bac145d8c591204dd91a59a14be3

--- Assembly-CSharp/DesktopDefaultKeyboardMapping.cs
+++ Assembly-CSharp/DesktopDefaultKeyboardMapping.cs
@@ -293,6 +293,7 @@ public class DesktopDefaultKeyboardMapping : IKogamaInputMap
 		{
 			controlDown[control] = true;
 		}
+		bool result = controlDown[control];
 		bool flag2 = KeyUp(control);
 		if (flag2)
 		{
@@ -301,7 +302,7 @@ public class DesktopDefaultKeyboardMapping : IKogamaInputMap
 		switch (keyState)
 		{
 		case KeyState.Pressed:
-			return controlDown[control];
+			return result;
 		case KeyState.Down:
 			return flag;
 		case KeyState.Up:
@@ -327,20 +328,15 @@ public class DesktopDefaultKeyboardMapping : IKogamaInputMap
 
 	private bool KeyUp(KogamaControls control)
 	{
-		bool result = false;
 		KeyCode[] array = keyMapping[control];
 		foreach (KeyCode key in array)
 		{
-			if (Input.GetKey(key))
+			bool flag = controlDown[control] && !Input.GetKey(key);
+			if (!Input.GetKeyUp(key) && !flag)
 			{
-				result = false;
-				break;
-			}
-			if (Input.GetKeyUp(key))
-			{
-				result = Input.GetKeyUp(key);
+				return false;
 			}
 		}
-		return result;
+		return true;
 	}
 }
